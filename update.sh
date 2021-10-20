044202ea48d116f81cff20ecf64b810cf8f71fbc
Saad allah Samadi <ssamadi@e1r9p4.1337.ma>
2021-10-20 19:07:09 +0100

    f035f04b5d2e841d89b4

--- update.sh
+++ update.sh
@@ -5,14 +5,12 @@ do
 	git add .
 
 	a=$(echo $RANDOM | md5 | head -c 20)
-	b=$(read -t 5 -p "Enter your commit > ")
+	b=$(read -t 20 -p "Enter your commit > ")
 	if [ $? == 1 ]
 	then
 		git commit -m "$a"
 	else
 		git commit -m "$b"
 	fi
-	# echo ${REPLY} $? $a
-	# git commit -m "$a"
 	git push
 done
