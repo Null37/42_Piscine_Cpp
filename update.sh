0bfc109e2234f41386da8f8b62da81f349b10cd6
Saad allah Samadi <ssamadi@e1r9p4.1337.ma>
2021-10-20 19:05:08 +0100

    3dcd6cf6a4cae7c5aa3b

--- update.sh
+++ update.sh
@@ -1,9 +1,18 @@
 while [ 1 ]
 do
 	
-	sleep 300
-	git add 660.
+	sleep 10
+	git add .
+
 	a=$(echo $RANDOM | md5 | head -c 20)
-	git commit -m "$a"
+	b=$(read -t 5 -p "Enter your commit > ")
+	if [ $? == 1 ]
+	then
+		git commit -m "$a"
+	else
+		git commit -m "$b"
+	fi
+	# echo ${REPLY} $? $a
+	# git commit -m "$a"
 	git push
 done
