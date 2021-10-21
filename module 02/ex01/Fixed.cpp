09c2197cf06a5ff90ff09789559ecd5a9fb20fa2
Saad allah Samadi <ssamadi@e1r9p4.1337.ma>
2021-10-21 18:57:48 +0100

    final ex02

--- module 02/ex01/Fixed.cpp
+++ module 02/ex01/Fixed.cpp
@@ -39,3 +39,29 @@ void Fixed::operator=(const Fixed& old_fix)
     fixed_point = old_fix.fixed_point;
 }
 
+Fixed::Fixed(const int nb)
+{
+    std::cout << "Int constructor called" << "\n";
+    fixed_point = roundf(nb * (float)(1 << fractional_bits));
+}
+
+Fixed::Fixed(const float nf)
+{
+    std::cout << "Float constructor called" << "\n";
+    fixed_point = roundf(nf  * (float)(1 << fractional_bits)); // shift left use fractinal bits or power(2, 8)
+}
+
+float Fixed::toFloat( void ) const
+{
+    return (this->fixed_point / (float)(1 << fractional_bits)); // right shift 
+}
+
+int Fixed::toInt( void ) const
+{
+    return (this->fixed_point / (float)(1 << fractional_bits)); // right shift ex:10.0
+}
+ std::ostream& operator<<(std::ostream& os, const Fixed& obj)
+{
+   os << obj.toFloat();
+    return os;
+}
--- module 02/ex01/Fixed.hpp
+++ module 02/ex01/Fixed.hpp
@@ -2,7 +2,7 @@
 #define FIXED_HPP
 
 #include <iostream>
-
+#include <cmath>
 
 class Fixed
 {
@@ -16,10 +16,12 @@ class Fixed
         int getRawBits( void ) const;
         void setRawBits( int const raw );
         void operator=(const Fixed& old_fix);
-        Fixed(const int newint);
-        
+        Fixed(const int nb);
+        Fixed(const float nf);
+        float toFloat(void) const;
+        int toInt( void ) const;
 };
 
-
+std::ostream& operator<<(std::ostream& os, const Fixed& obj);
 
 #endif
