7a3f9e2549e6222abc7893e265a4e3f992eb651a
Saad allah Samadi <ssamadi@e1r9p4.1337.ma>
2021-10-21 09:22:43 +0100

    bce026efd1ab268088eb

---
+++ module 02/ex01/Fixed.hpp
@@ -0,0 +1,28 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+
+class Fixed
+{
+    private:
+        int fixed_point; 
+        static const int  fractional_bits;//8
+    public:
+        Fixed( void );
+        ~Fixed( void );
+        Fixed(const Fixed &old);
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+        void operator=(const Fixed& old_fix);
+        Fixed(const int cin
+        
+        
+        
+        )
+};
+
+
+
+#endif
--- update.sh
+++ update.sh
@@ -1,17 +1,9 @@
 while [ 1 ]
 do
 	
-	sleep 10
+	sleep 3600
 	git add .
 	a=$(echo $RANDOM | md5 | head -c 20)
-	b=$(read -t 5 -p "Enter your commit > ")
-	# echo $?
-	if [ $? == 0 ]
-	then
-		git commit -m "$b"
-	elif [ $? == 1 ]
-	then
-		git commit -m "$a"
-	fi
+	git commit -m "$a"
 	git push
 done
